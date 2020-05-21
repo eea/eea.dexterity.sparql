071dcb985b2d6406befa671785b5141beb20901a
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2020-05-21 18:14:26 +0300

    fixed pyflakes
    refs #111217

--- eea/dexterity/sparql/content/validators.py
+++ eea/dexterity/sparql/content/validators.py
@@ -6,7 +6,6 @@ from Products.ZSPARQLMethod.Method import run_with_timeout, interpolate_query
 from Products.ZSPARQLMethod.Method import query_and_get_result
 from plone.api.portal import getSite
 from zope.annotation import IAnnotations
-from zope.interface import implements
 from zope.globalrequest import getRequest
 
 
